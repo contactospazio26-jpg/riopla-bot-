915279a2d0af0425a92012b67c85078a6cf13897
contactospazio26-jpg <contacto.spazio26@gmail.com>
2025-09-17 09:54:06 -0300

    pasa api para afuera

--- mi-proyecto/api/index.ts
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  if (req.method === "POST") {
-    return res.status(200).json({ reply: "SI" });
-  }
-  return res.status(200).send("Webhook activo ✅");
-}
-
-
