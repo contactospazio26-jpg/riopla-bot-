c92930c871ef2ec67cfa02b70bada445e671fcb9
contactospazio26-jpg <contacto.spazio26@gmail.com>
2025-09-17 16:48:44 -0300

    ,LKJN

---
+++ index.js
@@ -0,0 +1,13 @@
+// index.js
+export default function handler(req, res) {
+  if (req.method === "POST") {
+    const body = req.body;
+    const message = body?.message || "mensaje vacío";
+
+    res.status(200).json({
+      reply: `Recibí tu mensaje: ${message}`
+    });
+  } else {
+    res.status(200).send("Webhook activo ✅");
+  }
+}
